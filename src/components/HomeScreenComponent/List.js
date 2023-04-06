08b140773c1b2f71a11ee18e86533838049cd62d
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-04-06 23:32:05 +0530

    added nav ref to the home screen list item and also extracted data frm route param

--- src/components/HomeScreenComponent/List.js
+++ src/components/HomeScreenComponent/List.js
@@ -1,35 +1,52 @@
-import {StyleSheet, Text, View, Image, FlatList, ActivityIndicator, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import {COLORS, SIZES} from '../../styles';
+import {useNavigation} from '@react-navigation/native';
+import { LOGGED_IN_SCREEN_NAME } from '../../constants/screenConstants';
 
 const ITEM_HEIGHT = SIZES.height * 0.5 - 80;
 const ITEM_WIDTH = SIZES.width * 0.9;
+let navRef;
 
 const renderItem = ({item}) => {
-  const {population,region,capital} = item;
- return(
-  <TouchableOpacity style={styles.renderItemContainer}>
-  <Image source={{uri: item.flags.png}} style={styles.flagStyle} />
-  <View>
-    <Text numberOfLines={1} style={styles.countryNameStyle}>{item.name.common}</Text>
-    <Text style={styles.populationTextStyle}>
-      <Text style={styles.labelStyle}>Population: </Text>{population}
-    </Text>
-    <Text style={styles.populationTextStyle}>
-      <Text style={styles.labelStyle}>Region: </Text>{region}
-    </Text>
-    <Text style={styles.populationTextStyle}>
-      <Text style={styles.labelStyle}>Capital: </Text>{capital}
-    </Text>
-
-  </View>
-</TouchableOpacity>
- )
+  const {population, region, capital} = item;
+  return (
+    <TouchableOpacity onPress={()=>navRef.navigate(LOGGED_IN_SCREEN_NAME.details,{item:item})} style={styles.renderItemContainer}>
+      <Image source={{uri: item.flags.png}} style={styles.flagStyle} />
+      <View>
+        <Text numberOfLines={1} style={styles.countryNameStyle}>
+          {item.name.common}
+        </Text>
+        <Text style={styles.populationTextStyle}>
+          <Text style={styles.labelStyle}>Population: </Text>
+          {population}
+        </Text>
+        <Text style={styles.populationTextStyle}>
+          <Text style={styles.labelStyle}>Region: </Text>
+          {region}
+        </Text>
+        <Text style={styles.populationTextStyle}>
+          <Text style={styles.labelStyle}>Capital: </Text>
+          {capital}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const keyExtractor = (item, index) => index.toString();
 
-const List = ({data,endReachedCall, loading}) => {
+const List = ({data, endReachedCall, loading}) => {
+  const navigation = useNavigation();
+  navRef = navigation;
 
   return (
     <FlatList
@@ -49,13 +66,18 @@ const styles = StyleSheet.create({
   renderItemContainer: {
     height: ITEM_HEIGHT,
     width: ITEM_WIDTH,
-    backgroundColor:COLORS.white,
+    backgroundColor: COLORS.white,
     padding: 10,
-    elevation:2,
-    marginVertical:10
+    elevation: 2,
+    marginVertical: 10,
   },
   flagStyle: {width: ITEM_WIDTH - 20, height: ITEM_HEIGHT * 0.5},
-  countryNameStyle: {marginTop:20,fontSize: 20, fontWeight: 'bold', color:COLORS.black},
-  populationTextStyle: {paddingVertical:5,fontSize: 14, color: COLORS.grey},
-  labelStyle:{fontSize:14, fontWeight:"bold", color:"black"}
+  countryNameStyle: {
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: COLORS.black,
+  },
+  populationTextStyle: {paddingVertical: 5, fontSize: 14, color: COLORS.grey},
+  labelStyle: {fontSize: 14, fontWeight: 'bold', color: 'black'},
 });
--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,7 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useRoute } from '@react-navigation/native';
 
 const DetailsScreen = () => {
+  const route = useRoute();
+  const { item } = route.params;
+  {console.log(item.name)}
   return (
     <View>
       <Text>DetailsScreen</Text>
