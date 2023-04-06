abaa714ea75303a651f6f1fe1a7bdcfcb2ecb9c8
Ajaykumar Rajasekaran <ajaykumarrajasekaran@Ajaykumars-MacBook-Pro.local>
2023-04-07 03:38:17 +0530

    removed unused theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,17 +4,11 @@ const {height, width} = Dimensions.get('window');
 export const COLORS = {
   primary: '#007aff',
   secondary: '#6c757d',
-  success: '#28a745',
-  danger: '#dc3545',
-  warning: '#ffc107',
-  info: '#17a2b8',
   light: '#f2f2f2',
   dark: '#343a40',
   white: '#FFFFFF',
-  red: '#ff0000',
   blue: '#0000FF',
   grey: '#808080',
-  blueSecondary: '#8FB1CC',
   black: '#000000',
 };
 
